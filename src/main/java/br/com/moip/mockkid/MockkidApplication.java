c59390c62d50a17a94dde9462ea44c88d30cc12a
zyondias <zyon.dias@hotmail.com>
2017-11-10 10:37:27 -0200

    Second commit

---
+++ src/main/java/br/com/moip/mockkid/MockkidApplication.java
@@ -0,0 +1,12 @@
+package br.com.moip.mockkid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockkidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockkidApplication.class, args);
+	}
+}
---
+++ src/test/java/br/com/moip/mockkid/MockkidApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.moip.mockkid;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MockkidApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
