f3bedc75003601a2eb01416ca291eec607b9e9eb
zyondias <zyon.dias@hotmail.com>
2017-11-10 11:35:42 -0200

    create basic structure to pages html

--- src/main/java/br/com/moip/mockkid/controller/MockKidController.java
+++ src/main/java/br/com/moip/mockkid/controller/MockKidController.java
@@ -1,7 +1,9 @@
 package br.com.moip.mockkid.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Created by zyon.silva on 11/10/17.
@@ -9,9 +11,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class MockKidController {
 
-    @RequestMapping("/mockkid")
-    public String pageAdmin(){
+    @RequestMapping(path = "/mockkid", method= RequestMethod.GET)
+    public String admin(Model model) {
+        System.out.println("passei aqui mano aaaaaaa");
 
-        return "index";
+        model.addAttribute("message", "kkk eai man");
+
+        return "admin";
     }
 }
