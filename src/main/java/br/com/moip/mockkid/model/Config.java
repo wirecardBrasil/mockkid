5b93eccc497d1e4fc9001f9e1fd54fb5b319c394
Evandro Silvestre <evandro.silvestre@moipadmins-MacBook-Pro.local>
2017-11-10 14:04:38 -0200

    Parser do arquivo de configuracao

---
+++ src/main/java/br/com/moip/mockkid/model/Config.java
@@ -0,0 +1,13 @@
+package br.com.moip.mockkid.model;
+
+public class Config {
+	private Configuration configuration;
+
+	public Configuration getConfiguration() {
+		return configuration;
+	}
+
+	public void setConfiguration(Configuration configuration) {
+		this.configuration = configuration;
+	}
+}
