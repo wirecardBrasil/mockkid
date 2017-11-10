26d75eced06de0394b685bb1bf2439770de0b664
Evandro Silvestre <evandro.silvestre@moipadmins-MacBook-Pro.local>
2017-11-10 14:02:25 -0200

    Parser do arquivo de configuracao

--- src/main/java/br/com/moip/mockkid/MockkidApplication.java
+++ src/main/java/br/com/moip/mockkid/MockkidApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MockkidApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(MockkidApplication.class, args);
+
 	}
 }
--- src/main/java/br/com/moip/mockkid/br/com/moip/mockkid/configuration/ConfigParser.java
+++ src/main/java/br/com/moip/mockkid/br/com/moip/mockkid/configuration/ConfigParser.java
@@ -1,7 +1,45 @@
 package br.com.moip.mockkid.br.com.moip.mockkid.configuration;
 
+
+import br.com.moip.mockkid.model.Config;
+import br.com.moip.mockkid.model.Configuration;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+import java.io.File;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class ConfigParser {
 
+    private static Map<String, Configuration> configurations = new LinkedHashMap<>();
+
+    public static void main(String[] args) {
+    		ConfigParser.openConfigurations();
+    }
     
+    public static void openConfigurations(){
+        File f = new File(ConfigParser.class.getClassLoader().getResource("configuration/teste.yaml").getFile());
+        System.out.println(toConfiguration(f).getConfiguration().getEndpoint().getUrl());
+    }
+
+    public static Config toConfiguration(File file){
+        ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        Config config = null;
+        try {
+            config = mapper.readValue(file, Config.class);
+            System.out.println(ReflectionToStringBuilder.toString(config, ToStringStyle.MULTI_LINE_STYLE));
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return config;
+    }
+
 
 }
