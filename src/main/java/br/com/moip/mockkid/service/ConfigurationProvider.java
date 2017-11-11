0ab52361840b721d3f83c07c4c42ebc6ee3502d5
Eduardo Colabardini <eduardocolabardini@gmail.com>
2017-11-11 11:48:12 -0200

    Remove unused method and class

--- src/main/java/br/com/moip/mockkid/service/ConfigurationProvider.java
+++ src/main/java/br/com/moip/mockkid/service/ConfigurationProvider.java
@@ -1,19 +1,11 @@
 package br.com.moip.mockkid.service;
 
 import br.com.moip.mockkid.configuration.Configurations;
-import br.com.moip.mockkid.model.Conditional;
 import br.com.moip.mockkid.model.Configuration;
-import br.com.moip.mockkid.model.Endpoint;
-import br.com.moip.mockkid.model.Method;
-import br.com.moip.mockkid.model.Response;
-import br.com.moip.mockkid.model.ResponseConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
 
 @Component
 public class ConfigurationProvider {
@@ -29,23 +21,4 @@ public class ConfigurationProvider {
         return configurations;
     }
 
-    private Map<String, Configuration> mockConfiguration() {
-        Map<String, Configuration> configs = new HashMap<>();
-
-        Configuration configuration = new Configuration();
-
-        Endpoint endpoint = new Endpoint();
-        endpoint.setMethod(Method.GET);
-        endpoint.setUrl("/zyon");
-        configuration.setEndpoint(endpoint);
-
-        ResponseConfiguration responseConfiguration = new ResponseConfiguration();
-        responseConfiguration.setResponse(new Response());
-        responseConfiguration.setConditional(new Conditional());
-        configuration.setResponseConfigurations(Arrays.asList(responseConfiguration));
-
-        configs.put("/zyon", configuration);
-
-        return configs;
-    }
 }
